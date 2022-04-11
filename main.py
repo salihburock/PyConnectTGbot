daeb8cda2426703062f8d63c6d9292b9313a9076
Salih <94194901+salihburock@users.noreply.github.com>
2022-04-11 23:22:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import telebot
 import pyautogui
 from PIL import Image, ImageDraw, ImageFont
 import os
+import codecs
 import subprocess as sp
 api_key = open('api','r+').read()
 bot = telebot.TeleBot(api_key)
@@ -17,16 +18,17 @@ def test(message):
 def mp(message):
     pyautogui.screenshot('ss.png')
     ssimage = Image.open('ss.png')
-
     xpos = pyautogui.position()[0]
     ypos = pyautogui.position()[1]
-
     draw = ImageDraw.Draw(ssimage)
-
     draw.rectangle((xpos, ypos, xpos + 20, ypos + 20), fill=True, outline='green', width=1)
     ssimage.save('ssout.png')
     bot.send_message(text=str(pyautogui.position()),chat_id=message.chat.id)
     bot.send_photo(message.chat.id, open('ssout.png','rb'))
+
+@bot.message_handler(commands=['test','t'])
+def test(message):
+    bot.send_message(chat_id=message.chat.id,text=message)
 
 @bot.message_handler(commands=['moveTo','moveto'])
 def ptmove(message):
@@ -51,20 +53,7 @@ def ptmove(message):
     except:
         bot.send_message(text="usage: /move x y")
 
-@bot.message_handler(commands=['os','cmd','shell'])
-def oscmd(message):
-    msg = message.text
-    bot.send_message(text="Shell:",chat_id=message.chat.id)
-    while msg != "exit":
-        if msg == "/os" or msg == "/cmd" or msg == "/shell":
-            pass
-        else:
-            @bot.message_handler()
-            def oscmdmode(message):
-                outpt = sp.check_output(msg,shell=True)
-                bot.reply_to(message=message,text=outpt)
-    bot.send_message(text="Exiting shell...",chat_id=message.chat.id)
-    
+
 @bot.message_handler(commands=['type'])
 def ptwrite(message):
     inpt = str(message.text).split('/type ')[1]
@@ -90,9 +79,9 @@ def ptclick(message):
         y = tinp[2]
         pyautogui.click(x,y)
 
-@bot.message_handler(commands=['lclick','lc'])
+@bot.message_handler(commands=['rclick','rc'])
 def ptclick(message):
-    if message.text == "/lclick":
+    if message.text == "/rclick":
         pyautogui.leftClick()
     else:
         tinp = message.text.split(' ')
@@ -122,6 +111,37 @@ def ptdclick(message):
         x = tinp[1]
         y = tinp[2]
         pyautogui.doubleClick(x,y)
+@bot.message_handler(commands=['os','cmd','shell'])
+def oscmd(message):
+    bot.send_message(text="Shell:",chat_id=message.chat.id)
+    osmode = True
 
+    while osmode:
+        @bot.message_handler()
+        def oscmdmode(message):
+
+            msg = str(message.text)
+            osmode = True
+            print(msg)
+            if msg == "exit":
+                    osmode = False
+                    bot.send_message(chat_id=message.chat.id, text="Exiting Shell")
+                    
+                    exit
+            if osmode:
+                if not msg.startswith('/'):
+                    try:
+                        outpt = sp.check_output(msg,shell=True)
+                        outpt = codecs.decode(outpt, 'UTF-8')
+                        bot.reply_to(message=message, text=outpt)
+                        print(outpt)
+                    except UnicodeDecodeError:
+                        outpt = sp.check_output(msg,shell=True)
+                        outpt = str(outpt)
+                        bot.reply_to(message=message, text=outpt)
+                        print(outpt)
+                else:
+                    pass
+                
 print('Bot Started')
 bot.polling(non_stop=True)
